f5cddd736c55b6a7ce2a3d035f244d22572c9ff6
US\mabhang <mabhang@deloitte.com>
2018-11-26 10:04:40 +0530

    paytmcode

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+var checksum  = require('./checksum');
 var express = require('express');
 var app  = new express();
-app.get('/api/course',(req,res) =>{
-    res.send('hello world');
+app.use(express.json());
+app.post('/api/getchecksum',(req,response) =>{
+    console.log('called from  postman'+ req.body.mid);
+    var params 						= {};
+    params['MID'] 					= req.body.mid;
+    params['WEBSITE']				= 'webstaging';
+    params['CHANNEL_ID']			= 'WEB';
+    params['INDUSTRY_TYPE_ID']	= 'Retail';
+    params['ORDER_ID']			= 'TEST_'  + new Date().getTime();
+    params['CUST_ID'] 			= 'Customer001';
+    params['TXN_AMOUNT']			= req.body.taxamount;
+    params['CALLBACK_URL']		= 'http://localhost:'+port+'/callback';
+    params['EMAIL']				= req.body.email;
+    params['MOBILE_NO']			= req.body.mobilen;
+
+    var mkey = req.body.mkey;
+    checksum.genchecksum(params,mkey, function (err, res) {
+    console.log('sddfhyrtty');
+     if(!err){
+            return response.json(res)
+       }
+    });
+
+
 });
 const port = process.env.PORT || 3000;
 app.listen(port  , () => console.log(`listeing to po ${port}`));
